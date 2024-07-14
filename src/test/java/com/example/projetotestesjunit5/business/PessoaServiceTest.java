e220a5a13a907553d5e3adb8f3e552bc32255421
Andre Cabalo <andreccabalo@gmail.com>
2024-07-14 14:41:53 -0300

    Adicionando comentários

--- src/test/java/com/example/projetotestesjunit5/business/PessoaServiceTest.java
+++ src/test/java/com/example/projetotestesjunit5/business/PessoaServiceTest.java
@@ -23,43 +23,33 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class) //para rodar os testes com o mockito
 public class PessoaServiceTest {
-
     @InjectMocks
     PessoaService service;
-
     @Mock //cria um mock do repository, mock serve para simular o comportamento de um objeto
     PessoaRepository repository;
-
     Pessoa pessoa;
-
     //mockando pessoa para ser usada antes de cada teste
     @BeforeEach
     public void setUp() {
         pessoa = new Pessoa("Angelica", "12358569852", "Desenvolvedora", 30, "Sao Paulo", "Rua das Cruzes", 54);
     }
-
     //Annotation @Test da JUNIT.JUPITER e não apenas JUNIT
     @Test
     void deveBuscarPessoasPorCPFComSucesso() {
         //cenário
         when(repository.findPessoa(pessoa.getCpf())).thenReturn(Collections.singletonList(pessoa));
-
         //execução
         List<Pessoa> pessoas = service.buscaPessoasPorCpf(pessoa.getCpf());
-
         //verificação
         assertEquals(Collections.singletonList(pessoa), pessoas);
         verify(repository).findPessoa(pessoa.getCpf());
         verifyNoMoreInteractions(repository);
-
     }
-
     @Test
     void naoDeveChamaroRepositoryCasoParametroCPFNulo(){
         final BusinessException e = assertThrows(BusinessException.class, () -> {
             service.buscaPessoasPorCpf(null);
         });
-
         //verificando se a exceção foi lançada
         assertThat(e, notNullValue());
         //verificando a mensagem da exceção
@@ -70,9 +60,7 @@ public class PessoaServiceTest {
         assertThat(e.getCause().getMessage(), is("Cpf é obrigatório!"));
         //verifica se não houve interação com o repository
         verifyNoInteractions(repository);
-
     }
-
     @Test
     void deveAcionarExceptionQuandoRepositoryFalhar() {
         when(repository.findPessoa(pessoa.getCpf())).thenThrow(new RuntimeException("Falha ao buscar pessoas por cpf!"));
@@ -80,7 +68,6 @@ public class PessoaServiceTest {
         final BusinessException e = assertThrows(BusinessException.class, () -> {
             service.buscaPessoasPorCpf(pessoa.getCpf());
         });
-
         //verificando se a exceção foi lançada
         assertThat(e.getMessage(), is(format("Erro ao buscar pessoas por cpf = %s", pessoa.getCpf())));
         //verificando se a causa da exceção é uma RuntimeException
@@ -91,9 +78,5 @@ public class PessoaServiceTest {
         verify(repository).findPessoa(pessoa.getCpf());
         //verifica se não houve mais interações com o repository
         verifyNoMoreInteractions(repository);
-
     }
-
-
-
 }
